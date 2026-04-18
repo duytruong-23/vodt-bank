3b09c65e46844a3c02279413b994383ab0cca94a
duytruong-23 <zduytruongz@gmail.com>
2026-04-18 17:04:19 +0700

    Add custom access denied handler and authentication entry point for error responses

---
+++ src/main/java/com/example/vodtbank/config/CustomAccessDeniedHandler.java
@@ -0,0 +1,31 @@
+package com.example.vodtbank.config;
+
+import java.io.IOException;
+
+import com.example.vodtbank.response.Response;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import tools.jackson.databind.ObjectMapper;
+
+@Configuration
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+	private final ObjectMapper mapper;
+
+	public CustomAccessDeniedHandler(ObjectMapper mapper) {
+		this.mapper = mapper;
+	}
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			AccessDeniedException accessDeniedException) throws IOException {
+		Response<?> responseBody = new Response<>(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage(),
+				null);
+
+		response.setContentType("application/json");
+		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+		response.getWriter().write(mapper.writeValueAsString(responseBody));
+	}
+}
---
+++ src/main/java/com/example/vodtbank/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.example.vodtbank.config;
+
+import java.io.IOException;
+
+import com.example.vodtbank.response.Response;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import tools.jackson.databind.ObjectMapper;
+
+@Configuration
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	private final ObjectMapper mapper;
+
+	public CustomAuthenticationEntryPoint(ObjectMapper mapper) {
+		this.mapper = mapper;
+	}
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+		Response<?> responseBody = new Response<>(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage(), null);
+
+		response.setContentType("application/json");
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		response.getWriter().write(mapper.writeValueAsString(responseBody));
+	}
+}
